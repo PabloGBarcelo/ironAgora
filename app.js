fd580a916ff3b9526c842cd2baccd1d894cf014c
Pablo Gutierrez Barcelo <pablo@gutierrezbarcelo.com>
2017-11-20 18:44:41 +0100

    added axios and main-layout

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ const app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.set('layout', 'layouts/main-layout');
+app.use(expressLayouts);
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
