6a7b4f34e93fc112de1ecd585c6f112d9e608cfb
sdediego <sergio.dediego@outlook.com>
2017-11-23 00:15:58 +0100

    Formated dates with momentjs

--- routes/forum.js
+++ routes/forum.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const moment = require('moment');
 const Question = require('../models/Question');
 
 router.get('/', (req, res, next) => {
   Question.find({}, null, {sort: {created_at: -1}}) // desc
           .populate('_authorId')
           .exec()
-          .then((results) => { res.render('forum/index', {results} );})
+          .then((results) => { res.render('forum/index', {results, moment} ); })
           .catch((err) => console.log(err));
 });
 
