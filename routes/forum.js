4ced6b99c5663b7d9cad3c1cdb0c6fcb90346675
Pablo Gutierrez Barcelo <pablo@gutierrezbarcelo.com>
2017-11-22 19:21:09 +0100

    all working with tags

--- routes/forum.js
+++ routes/forum.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Question = require('../models/Question');
 
 router.get('/', (req, res, next) => {
-  Question.find({})
+  Question.find({}, null, {sort: {created_at: -1}}) // desc
           .populate('_authorId')
           .exec()
           .then((results) => { res.render('forum/index', {results} );})
