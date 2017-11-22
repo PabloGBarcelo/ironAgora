62c0fd6591924df22910c65bca9d887f5601872a
Pablo Gutierrez Barcelo <pablo@gutierrezbarcelo.com>
2017-11-22 16:10:58 +0100

    fix forum.js

--- routes/forum.js
+++ routes/forum.js
@@ -3,16 +3,13 @@ const router = express.Router();
 const Question = require('../models/Question')
 
 router.get('/', (req, res, next) => {
-<<<<<<< HEAD
   console.log(req.user);
   res.render('forum/index');
-=======
   Question.find({})
           .populate('_authorId')
           .exec()
           .then((results) => { res.render('forum/index', {results} )})
           .catch((err) => console.log(err))
->>>>>>> ea53babd2b116be3b40263aba563adceb50c26c3
 });
 
 module.exports = router;
