e7681f403c2baebb5799bacbbed8b04da754847a
Pablo Gutierrez Barcelo <pablo@gutierrezbarcelo.com>
2017-11-22 18:15:39 +0100

    edition and show finish

--- routes/question.js
+++ routes/question.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Question = require('../models/Question')
+const Question = require('../models/Question');
 
 router.get('/new', (req, res, next) => {
   console.log(req.user);
@@ -22,8 +22,32 @@ router.post('/new', (req, res, next) => {
     isSurvey: false
   });
   newQuestion.save((err) =>{
-    res.redirect('/forum')
+    res.redirect('/forum');
   });
+});
+
+router.get('/:id/edit', (req, res, next) => {
+  Question.findById(req.params.id)
+          .then((results) => res.render('question/edit', { medium: 1, results}))
+          .catch((err) => console.log(err));
+});
+
+router.post('/:id/edit', (req, res, next) => {
+  let mainPhoto = req.body.myPost.split(/img src="/)[1].split(/"/)[0];
+  let editPost = {
+    title: req.body.myTitle,
+    content: req.body.myPost,
+    mainPhoto
+  };
+  Question.findByIdAndUpdate(req.params.id, editPost)
+          .then((results) => res.redirect('/forum/'))
+          .catch((err) => console.log(err));
+});
+
+router.get('/:id/show', (req, res, next) => {
+  Question.findById(req.params.id)
+          .then((results) => res.render('question/show',  results ))
+          .catch((err) => console.log(err));
 });
 
 module.exports = router;
