94344de64f365c751028bcf46a1168074a411f43
sdediego <sergio.dediego@outlook.com>
2017-11-23 13:07:51 +0100

    Logged out implemented

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,32 +1,23 @@
 var checkNewContent = function() {
   setInterval(() => {
-    //axios.get('http://localhost:300/forum/check')
-    //  .then(response => {
-    //    var numCurrentQuest = document.getElementsByClassName('onePost').length;
-    //    console.log(numCurrentQuest, response);
-    //    if (numCurrentQuest - response > 0) {
-    //      $('.allPosts:first').before(`<button class="new-fetch" type="submit">+${diffQuestions} new posts</button>`);
-    //    }
-    //  })
-    //  .catch(error => {
-    //    console.log(error);
-    //  });
     $.ajax({
       method: 'GET',
       url: 'http://localhost:3000/forum/check',
-      cache: false,
       dataType: 'json',
       success: response => {
         let numResponse = response['results'].length;
         let numCurrentQuest = document.getElementsByClassName('onePost').length;
-        console.log(numCurrentQuest,response['results'].length);
-        let diffQuestions = response['results'].length - numCurrentQuest;
-        if (diffQuestions> 0) {
+        let diffQuestions = numResponse - numCurrentQuest;
+        if (diffQuestions > 0) {
+          let refreshButton = $('.new-fetch');
+          if (refreshButton) {
+            refreshButton.remove();
+          }
           $('.allPosts:first').before(`<button class="new-fetch" type="submit">+${diffQuestions} new posts</button>`);
         }
       },
-      error: err => {
-        console.log(err);
+      error: error => {
+        console.log(error);
       }
     });
   }, 3000);
@@ -39,7 +30,7 @@ $(document).ready(() => {
 
   $('.new-fetch').on('click', (event) => {
     event.preventDefault();
-    location.reload(true);
+    window.location.reload(true);
   });
 
 });
